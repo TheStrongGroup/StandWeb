c6b8fbf3d3a63d431c9b15ee2658d5e3dd3180eb
Fight4benben <zhangbenclass3@126.com>
2017-05-10 20:42:04 +0800

    增加app/js/main.js文件

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+/**
+* @author Zhangben 
+* @date 2017-04-23 17:15
+* @description Js code for main page.
+*  	name the id: "BarChart":过去31天用电,"LineChart":24小时负荷曲线
+* 	"OverView":能耗概览
+*/
+
+jQuery(document).ready(function($) {
+	
+});
