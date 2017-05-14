3e9431312532bfeae5232cf61dc72c8a9b13fd17
Fight4benben <zhangbenclass3@126.com>
2017-05-14 21:15:39 +0800

    在stand/home_page.jsp文件下增加id用于jquery选择，在home_page.jsp对应的main.js中填充了部分数据（用能概览）

--- app/js/main.js
+++ app/js/main.js
@@ -6,8 +6,107 @@
 * 	"OverView":能耗概览
 */
 
+var Home = (function(){
+	function _home(){
+
+		this.getData=function(url,params){
+			initDOM();
+			showDate();
+			$.getJSON(url, params, function(data) {
+				console.log(data);
+				showCompareData(data.energySurvey);
+			});
+		}
+
+		function initDOM(){
+			$("#dayConsumption .red").html("--");
+			$("#dayConsumption .grey").html("--");
+
+			$("#weekConsumption .red").html("--");
+			$("#weekConsumption .grey").html("--");
+
+			$("#monthConsumption .red").html("--");
+			$("#monthConsumption .grey").html("--");
+
+			$("#yearConsumption .red").html("--");
+			$("#yearConsumption .grey").html("--");
+		}
+
+		function showCompareData(data){
+			if(data.hasOwnProperty('today') && data.today != null)
+				$("#dayConsumption .red").html(data.today);	
+
+			if(data.hasOwnProperty('yesterday') && data.yesterday != null)
+				$("#dayConsumption .grey").html(data.yesterday);
+
+			if(data.hasOwnProperty('thisweek') && data.thisweek != null)
+				$("#weekConsumption .red").html(data.thisweek);	
+
+			if(data.hasOwnProperty('lastweek') && data.lastweek != null)
+				$("#weekConsumption .grey").html(data.lastweek);
+
+			if(data.hasOwnProperty('thismonth') && data.thismonth != null)
+				$("#monthConsumption .red").html(data.thismonth);	
+
+			if(data.hasOwnProperty('lastmonth') && data.lastmonth != null)
+				$("#monthConsumption .grey").html(data.lastmonth);
+
+			if(data.hasOwnProperty('thisyear') && data.thisyear != null)
+				$("#yearConsumption .red").html(data.thisyear);	
+
+			if(data.hasOwnProperty('lastyear') && data.lastyear != null)
+				$("#yearConsumption .grey").html(data.lastyear);
+
+		}
+
+		function showDate(){
+			var date = new Date();
+			var year = date.getFullYear();
+			var month = date.getMonth()+1;
+			var monthDay = date.getDate();
+			var weekDay = date.getDay();
+			var weekDayCh;
+
+			switch(weekDay){
+				case 0:
+					weekDayCh="周日";
+				break;
+				case 1:
+					weekDayCh="周一";
+				break;
+				case 2:
+					weekDayCh="周二";
+				break;
+				case 3:
+					weekDayCh="周三";
+				break;
+				case 4:
+					weekDayCh="周四";
+				break;
+				case 5:
+					weekDayCh="周五";
+				break;
+				case 6:
+					weekDayCh="周六";
+				break;
+			}
+
+
+			$("#currentDate>p").eq(0).html(year+"年"+month+"月"+monthDay+"日");
+
+			$("#currentDate>p").eq(1).html(weekDayCh);
+		}
+
+	};
+
+	return _home;
+})();
+
 jQuery(document).ready(function($) {
 	
 	$("#fistpage").addClass('active');
+
+	var home = new Home();
+	home.getData("rest/energysurvey/main");
 
 });
