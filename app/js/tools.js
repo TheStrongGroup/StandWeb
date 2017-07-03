623af2bf66d1a6b312b7c752d9a1dbaab2c802bb
Fight4benben <zhangbenclass3@126.com>
2017-07-03 19:41:47 +0800

    分类页面已完成，请更新

--- app/js/tools.js
+++ app/js/tools.js
@@ -15,7 +15,6 @@ var Stand={
 	selectAppender:function(data,$select,displayName,valueName){
 		$select.html("");
 		$.each(data,function(key,val){
-			console.log(val);
 			$select.append("<option value="+val[valueName]+">"+val[displayName]+"</option>");
 		});
 		return this;//允许使用连缀方式对多个select标签进行填充
